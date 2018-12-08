505344de74cdf00cdcc6b6dc4e39eca0d888fc73
Nate West <nwest@Nates-MacBook-Pro.local>
2018-12-08 14:44:45 -0500

    Cleanup all the things

--- adventofcode.hs
+++ adventofcode.hs
@@ -37,19 +37,13 @@ hamming :: String -> String -> Int
 hamming s1 = let f = map ord in sum . normalize . zipWith (-) (f s1) . f
 
 combinations :: [a] -> [(a, a)]
-combinations [] = []
-combinations (x:xs) = pairAll x xs ++ combinations xs
-                        where pairAll _ [] = []
-                              pairAll a (y:ys) = (a, y) : pairAll a ys
-
-combinations' :: [a] -> [(a, a)]
-combinations' xs = concat [ zip (repeat b) . drop a $ xs | (a, b) <- zip [1..] xs ]
+combinations xs = concat [ zip (repeat b) . drop a $ xs | (a, b) <- zip [1..] xs ]
 
 third :: (a, b, c) -> c
 third (_, _, c) = c
 
 numberTwoB :: IO String
-numberTwoB = commonLetters . head . sortOn third . map distance . combinations' . lines <$> readFile "/Users/nwest/2"
+numberTwoB = commonLetters . head . sortOn third . map distance . combinations . lines <$> readFile "/Users/nwest/2"
                where distance set@(l, r) = (l, r, uncurry hamming set)
                      commonLetters (s1, s2, _) = concat $ zipWith (\a b -> if a == b then [a] else "") s1 s2
 
@@ -59,13 +53,7 @@ type XCoord = Int
 type YCoord = Int
 type Width = Int
 type Height = Int
-type Coordinate = (Int, Int)
 data Plan = Plan XCoord YCoord Width Height deriving (Eq, Show)
-
-coordinates :: Plan -> [(Int, Int)]
-coordinates (Plan x y w h) = let top = y + h
-                                 right = x + w
-                             in [(x, y), (x, top), (right, y), (right, top)]
 
 splitOn :: Char -> String -> [String]
 splitOn c s | not (c `elem` s) = [s]
@@ -74,13 +62,22 @@ splitOn c s | not (c `elem` s) = [s]
 
 parsePlan :: String -> Plan
 parsePlan s = let [init -> a, b] = drop 2 . words $ s 
-                  [xc, yc] = f ',' a
+                  [x, y] = f ',' a
                   [w, h] = f 'x' b
-              in Plan xc yc w h
+              in Plan x y w h
   where
     f x = map read . splitOn x
 
-numberThree :: IO [Plan]
-numberThree = map parsePlan . lines <$> readFile "/Users/nwest/3"
+overlappingArea :: Plan -> Plan -> Int
+overlappingArea (Plan x1 y1 w1 h1) (Plan x2 y2 w2 h2) = let left = max x1 x2
+                                                            right = min (x1 + w1) (x2 + w2)
+                                                            bottom = max y1 y2
+                                                            top = min (y1 + h1) (y2 + h2)
+                                                            iWidth = max 0 (right - left)
+                                                            iHeight = max 0 (top - bottom)
+                                                        in  iWidth * iHeight
+
+numberThree :: IO Int
+numberThree = sum . map (uncurry overlappingArea) . combinations . map parsePlan . lines <$> readFile "/Users/nwest/3"
 
 -----------------------------------------------
